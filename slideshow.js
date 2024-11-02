722c1adf533d7c9caf5faa52ca3d26c5ff69b92f
sheep0363 <67054103+sheep0363@users.noreply.github.com>
2024-11-02 14:17:54 +0900

    ファビコン更新

--- slideshow.js
+++ slideshow.js
@@ -16,7 +16,7 @@ var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0
 //Vegas全体の設定
 
 $('#slider').vegas({
-		overlay: true,//画像の上に網線やドットのオーバーレイパターン画像を指定。
+		overlay: false,//画像の上に網線やドットのオーバーレイパターン画像を指定。
 		transition: 'blur',//切り替わりのアニメーション。http://vegas.jaysalvat.com/documentation/transitions/参照。fade、fade2、slideLeft、slideLeft2、slideRight、slideRight2、slideUp、slideUp2、slideDown、slideDown2、zoomIn、zoomIn2、zoomOut、zoomOut2、swirlLeft、swirlLeft2、swirlRight、swirlRight2、burnburn2、blurblur2、flash、flash2が設定可能。
 		transitionDuration: 2000,//切り替わりのアニメーション時間をミリ秒単位で設定
 		firstTransitionDuration: 10,
