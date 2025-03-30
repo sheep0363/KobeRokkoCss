40e659cfcf3b6d9ff9bcbe84ed641843e3caf8ae
sheep0363 <67054103+sheep0363@users.noreply.github.com>
2025-03-30 16:49:16 +0900

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -18,7 +18,7 @@ var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0
 $('#slider').vegas({
 		overlay: '/js/vegas/dist/overlays/06.png',//画像の上に網線やドットのオーバーレイパターン画像を指定。
 		transition: 'blur',//切り替わりのアニメーション。http://vegas.jaysalvat.com/documentation/transitions/参照。fade、fade2、slideLeft、slideLeft2、slideRight、slideRight2、slideUp、slideUp2、slideDown、slideDown2、zoomIn、zoomIn2、zoomOut、zoomOut2、swirlLeft、swirlLeft2、swirlRight、swirlRight2、burnburn2、blurblur2、flash、flash2が設定可能。
-		transitionDuration: 1000,//切り替わりのアニメーション時間をミリ秒単位で設定
+		transitionDuration: 2000,//切り替わりのアニメーション時間をミリ秒単位で設定
 		firstTransitionDuration: 10,
 		delay: 5000,//スライド間の遅延をミリ秒単位で。
 		animationDuration: 5000,//スライドアニメーション時間をミリ秒単位で設定
